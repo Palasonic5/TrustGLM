d8c1a1647d204e9abcd7bf705c2aac80ded0bfb7
Xinyue <144093752+Xinyue-Sheng@users.noreply.github.com>
2025-03-02 22:11:36 -0500

    Update eval.sh

--- model/LLaGA/eval/eval.sh
+++ model/LLaGA/eval/eval.sh
@@ -1,27 +1,28 @@
-model_path="Baselines/LLaGA/checkpoints"
+#!/usr/bin/env bash
 
-model_base="Baselines/LLaGA/vicuna-7b-v1.5-16k" #meta-llama/Llama-2-7b-hf
-mode="v1" # use 'llaga_llama_2' for llama and "v1" for others
-dataset="cora_sup" #test dataset
+MODEL_PATH="model/LLaGA/checkpoints"
+MODEL_BASE="model/LLaGA/vicuna-7b-v1.5-16k"  # or 'meta-llama/Llama-2-7b-hf'
+MODE="v1"            # use 'llaga_llama_2' for llama, or "v1" for others
+DATASET="cora_sup"   # test dataset
+PROMPT_FILE="model/LLaGA/dataset/sup/cora_sup/sampled_2_10_test_prbcd_local.jsonl"
+TASK="nc"            # test task
+EMB="sbert"
+USE_HOP=2            # 2 for ND, 4 for HO
+SAMPLE_SIZE=10
+TEMPLATE="ND"
 
-prompt_file="Baselines/LLaGA/dataset/sup/cora_sup/sampled_2_10_test_crossd_noisy_start_100_label_42.jsonl"
-task="nc" #test task
-emb="sbert"
-use_hop=2 # 2 for ND and 4 for HO
-sample_size=10
-template="ND"
-output_path="Baselines/LLaGA/output/${dataset}/${dataset}_${task}_${template}_cross_noisy_training_ln.txt"
+OUTPUT_PATH="model/LLaGA/output/${DATASET}/${DATASET}_${TASK}_${TEMPLATE}_prbcd_l.txt"
 
-python Baselines/LLaGA/eval/eval_pretrain.py\
-    --model_path ${model_path} \
-    --model_base ${model_base} \
-    --conv_mode  ${mode} \
-    --dataset ${dataset} \
-    --pretrained_embedding_type ${emb} \
-    --use_hop ${use_hop} \
-    --sample_neighbor_size ${sample_size} \
-    --answers_file ${output_path} \
-    --task ${task} \
+python model/LLaGA/eval/eval_pretrain.py \
+    --model_path "${MODEL_PATH}" \
+    --model_base "${MODEL_BASE}" \
+    --conv_mode  "${MODE}" \
+    --dataset "${DATASET}" \
+    --pretrained_embedding_type "${EMB}" \
+    --use_hop "${USE_HOP}" \
+    --sample_neighbor_size "${SAMPLE_SIZE}" \
+    --answers_file "${OUTPUT_PATH}" \
+    --task "${TASK}" \
     --cache_dir \
-    --template ${template}\
-    --prompt_file ${prompt_file}
+    --template "${TEMPLATE}" \
+    --prompt_file "${PROMPT_FILE}"
